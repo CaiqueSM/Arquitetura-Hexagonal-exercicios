c5f1b23ea8ad6c11d39de6b2e9fbdbd276269e49
caimachado <caique9machado@gmail.com>
2023-02-10 10:05:06 -0300

    Update: module-info

--- src/module-info.java
+++ src/module-info.java
@@ -5,15 +5,24 @@ module conta.sistema {
 
     // expondo porta de entrada (driver)
     exports conta.sistema.casouso.imp;
+    exports conta.sistema.casouso.porta;
 
     // expondo sistema negocio
     exports conta.sistema.dominio.servico;
+    exports conta.sistema.dominio.modelo;
 
     // expondo adaptadores de saídas (driven)
     exports conta.adaptador;
+    exports conta.sistema.porta;
 
     //expondo testes
     exports teste.unidade.dominio.modelo;
     exports teste.unidade.dominio.servico;
     exports teste.casouso;
+
+    //spring reflections
+    opens conta.sistema.casouso.porta;
+    opens conta.sistema.casouso.imp;
+    opens conta.sistema.dominio.servico;
+    opens conta.adaptador;
 }
